0a2814e75d1a15abc394a31388e48638ca166cbc
jeffrey <jeffrey.nasson@gmail.com>
2018-10-18 14:37:39 -0600

    third

---
+++ src/Components/SearchBar/AcctOrdersCartButtons/Parent.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+
+export default class Parent extends Component{
+    constructor(){
+        super()
+        this.state={}
+    }
+
+    render(){
+        return(
+            <div class='button-parent'></div>
+        )
+    }
+}
--- src/Components/SearchBar/Zipcode/Zip.js
+++ src/Components/SearchBar/Zipcode/Zip.js
@@ -3,7 +3,7 @@ import React,{Component} from 'react';
 export default function(){
     return(
         <div class='zip-container'>
-            <img src='/gpsicon.png'></img>
+            <img src='https://s3-us-west-1.amazonaws.com/cosmikdebris/8291bc67-88c9-4c7c-a8a8-acb53e862e2e-gpsicon.PNG'></img>
             <div class='zip-city'>Deliver to Provo 84601</div>
         </div>
     )
